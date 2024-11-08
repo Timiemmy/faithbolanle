a77d2bbd8b3773430e9bd90f82f7aa8219e57acf
Timiemmy <faith.bolanle@yahoo.com>
2024-11-08 17:28:39 +0100

    corrected staticfiles error

--- build_files.sh
+++ build_files.sh
@@ -2,9 +2,8 @@ echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
 mkdir -p .vercel/output/static
-cp -r staticfiles/ .vercel/output/static/
+cp -r staticfiles/* .vercel/output/static/
 
-python3 manage.py makemigrations
-python3 manage.py migrate
-
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
 echo "BUILD END"
